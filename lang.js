60aa6212cfb64354c55c14146001aa2bc7fceb4a
tropikade <victor.caplun777@gmail.com>
2026-07-04 11:21:15 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,53 @@
 
+const LANG = {
+    de: {
+        title: "Willkommen bei PotsdamNavi",
+        search: "Suche",
+        route: "Route planen",
+        favorites: "Favoriten",
+        reviews: "Bewertungen"
+    },
+
+    en: {
+        title: "Welcome to PotsdamNavi",
+        search: "Search",
+        route: "Plan Route",
+        favorites: "Favorites",
+        reviews: "Reviews"
+    },
+
+    ru: {
+        title: "Добро пожаловать в PotsdamNavi",
+        search: "Поиск",
+        route: "Построить маршрут",
+        favorites: "Избранное",
+        reviews: "Отзывы"
+    },
+
+    uk: {
+        title: "Ласкаво просимо до PotsdamNavi",
+        search: "Пошук",
+        route: "Побудувати маршрут",
+        favorites: "Обране",
+        reviews: "Відгуки"
+    },
+
+    tr: {
+        title: "PotsdamNavi'ye Hoş Geldiniz",
+        search: "Ara",
+        route: "Rota Oluştur",
+        favorites: "Favoriler",
+        reviews: "Yorumlar"
+    }
+};
+
+function getUserLanguage() {
+    let lang = navigator.language.substring(0,2);
+
+    if(LANG[lang])
+        return lang;
+
+    return "en";
+}
+
+const CURRENT_LANG = getUserLanguage();
